612364b4f2970d5cccce551c22e4f072b0362a5b
Lockey <1817811642@qq.com>
2022-06-03 18:10:12 +0800

    Blog_System

--- Blog_System/src/main/java/BlogDao.java
+++ Blog_System/src/main/java/BlogDao.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 //封装博客的基本操作
@@ -31,6 +32,7 @@ public class BlogDao {
     }
     //2、能够获取到博客表中的所有博客内容（获取博客列表页，并不是全部的内容）
     public List<Blog> selectAll() {
+        List<Blog> blogs = new ArrayList<>();
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
@@ -44,18 +46,60 @@ public class BlogDao {
                 blog.setBlogId(resultSet.getInt("blogId"));
                 blog.setTitle(resultSet.getString("title"));
                 blog.setContent(resultSet.getString("content"));
+                blog.setUserId(resultSet.getShort("userId"));
+                blog.setPostTime(resultSet.getTimestamp("postTime"));
+                blogs.add(blog);
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, statement, resultSet);
         }
-        return null;
+        return blogs;
     }
     //3、能够针对博客 id 获取到指定的博客内容（用于博客详情页）
     public Blog selectOne(int blogId) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "select * from where blogId = ?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, blogId);
+            resultSet = statement.executeQuery();
+
+            //此处是使用主键来判定的，所以就不用 while 了
+            if (resultSet.next()) {
+                Blog blog = new Blog();
+                blog.setBlogId(resultSet.getInt("blogId"));
+                blog.setTitle(resultSet.getString("title"));
+                blog.setContent(resultSet.getString("content"));
+                blog.setUserId(resultSet.getShort("userId"));
+                blog.setPostTime(resultSet.getTimestamp("postTime"));
+                return blog;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, statement, resultSet);
+        }
         return null;
     }
     //4、能够根据博客 id 删除博客
     public void delete(int blogId) {
-
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "delete from blog where blogId = ?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, blogId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, statement, null);
+        }
     }
 }
